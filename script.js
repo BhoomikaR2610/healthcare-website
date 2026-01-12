f3b8763e04276a3bee2c98ce6f15b5809e723454
Bhoomika <bhoomikaravibandary@gmail.com>
2026-01-12 20:51:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function appointmentMsg() {
+  document.getElementById("msg").innerText =
+    "Appointment booked successfully!";
+}
+
+function contactMsg() {
+  document.getElementById("contactMsg").innerText =
+    "Thank you! We will contact you soon.";
+}
